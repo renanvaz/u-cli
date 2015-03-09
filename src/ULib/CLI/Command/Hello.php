d5edd6de534cf930c85941f83b7ab3d76dfa78bf
Renan Vaz <renan.c.vaz@gmail.com>
2015-03-09 01:31:22 -0300

    Working...

---
+++ src/ULib/CLI/Command/Hello.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ULib\CLI\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Hello extends Command {
+    protected function configure() {
+        $this
+            ->setName('hello')
+            ->setDescription('Say hello');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $output->writeln('Hello World');
+    }
+}
---
+++ src/ULib/CLI/Command/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace UCli\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Test extends Command {
+    protected function configure() {
+        $this
+            ->setName('hello')
+            ->setDescription('Say hello');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $output->writeln('Hello World');
+    }
+}
